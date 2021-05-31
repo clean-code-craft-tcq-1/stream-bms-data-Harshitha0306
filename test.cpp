e68025b147a3ba89f0feae71610425b54d4ef794
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-05-31 18:49:08 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,38 +1,64 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "BMS_DataAcquisition.h"
 #include "BMS_DataSender.h"
 #include "stddef.h"
 #include "string.h"
 
 
-TEST_CASE("Test case to test the value within the Range")
-{
-  REQUIRE(isInRange(1,0,70)== 1);
-}
-
-
-TEST_CASE("Test case to test the value invaild Range")
-{
-  REQUIRE(isInRange(-100,-1,100)== 0);
-}
-
-
 TEST_CASE("Test for valid temperature values")
 {
-    struct Battery_Parameter_s Temperature_TC = {-40,80,255};
-    
-   REQUIRE(Get_BMSTemperatue(Temperature_TC)!= 255);
+  struct Battery_Parameter_s Temperature_s = {-40,80};
+
+REQUIRE((Get_BMSTemperatue(Temperature_s,1)[0] >= -40) & (Get_BMSTemperatue(Temperature_s,1)[0] <= 80));
 }
 
 
-TEST_CASE("Test for valid SOC values")
+TEST_CASE("Test for valid ChargeRate values")
 {
-  struct Battery_Parameter_s  ChargeRate_TC = {0,30,255};
-    
-  REQUIRE(Get_BMSChargeRate(ChargeRate_TC)!= 255);
+  struct Battery_Parameter_s ChargeRate_s = {0,30};
+
+  REQUIRE((Get_BMSChargeRate(ChargeRate_s,1)[0] >= 0) & (Get_BMSChargeRate(ChargeRate_s,1)[0] <= 30));
 }
+
+
+TEST_CASE("Test for valid Random values")
+{
+  float random_value = 0;
+  float min = 10;
+  float max = 50;
+  random_value = RandomNumGenerator(min,max);
+  REQUIRE(min <= random_value);
+  REQUIRE(max >= random_value);
+}
+
+
+TEST_CASE("Test for invalid Random values")
+{
+  float min = 50;
+  float max = 10;
+  REQUIRE(RandomNumGenerator(min,max) == 0);
+}
+
+
+TEST_CASE("Test for Successfull Send Operation of BMS paramaters")
+{
+    struct Battery_Parameter_s Temperature_s = {-40,80};
+    struct Battery_Parameter_s ChargeRate_s  = {10,30};
+    int MAXPARAMNUM =10;
+    
+REQUIRE(BMS_SendData(Temperature_s,ChargeRate_s,MAXPARAMNUM) == SENTSUCCESSFULLY);
+}
+
+TEST_CASE("Test to print paramters on console")
+{
+    struct Battery_Parameter_s Temperature_s = {-40,80};
+    struct Battery_Parameter_s ChargeRate_s  = {10,30};
+    int MAXPARAMNUM =10;
+    
+REQUIRE(BMS_SendData(Temperature_s,ChargeRate_s,MAXPARAMNUM) == SENTSUCCESSFULLY);
+}
+
 
 
 TEST_CASE("Test for valid Random values")
