4288b9ac1329fad95e543742b96ab4f0d118d852
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-05-31 18:48:02 +0530

    Update BMS_DataSender.h

--- BMS_DataSender.h
+++ BMS_DataSender.h
@@ -1,25 +1,34 @@
 /* *************************************************************************
-* File Name   :    BMS_DataSender.h
+* File Name   : BMS_DataSender.h
 * Description : Header file for BMS Sender type selection
 * ************************************************************************* */
+
 #ifndef BMS_DataSender_h
 #define BMS_DataSender_h
 
-#include "BMS_DataAcquisition.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /* ******************************* Defines ******************************* */
-struct Battery_Parameter_s Temperature_st;
- struct Battery_Parameter_s  ChargeRate_st;
+ struct Battery_Parameter_s
+{
+  float  Min_value;
+  float  Max_value;
+};
+
 typedef enum {
   SENDING_FAILED,
   SENTSUCCESSFULLY
 } SendStatus;
 
+
+
 /* ***************************** Prototypes ***************************** */
 
-SendStatus Print_ToConsole(float Temperature, float ChargeRate);
-SendStatus BMS_SendData(void);
+float * Get_BMSTemperatue(struct Battery_Parameter_s Temperature_s,int MAXPARAMNUM);
+float * Get_BMSChargeRate(struct Battery_Parameter_s ChargeRate_s,int MAXPARAMNUM);
+float RandomNumGenerator(float min,float max);
+SendStatus Output_ToConsole(float* Temperature, float* ChargeRate,int MAXPARAMNUM);
+SendStatus BMS_SendData(struct Battery_Parameter_s Temperature_s,struct Battery_Parameter_s ChargeRate_s,int MAXCOUNT);
 
-#endif
-
+#endif /* BMS_DataSender_h */
