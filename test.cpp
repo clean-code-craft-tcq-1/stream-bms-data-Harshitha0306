26e5a526c6fbd54ec78048e5157991a31c27fb5a
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-05-31 20:17:12 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,6 @@
 #include "stddef.h"
 #include "string.h"
 
-
 TEST_CASE("Test for valid temperature values")
 {
   struct Battery_Parameter_s Temperature_s = {-40,80};
@@ -58,32 +57,4 @@ TEST_CASE("Test to print paramters on console")
     
 REQUIRE(BMS_SendData(Temperature_s,ChargeRate_s,MAXPARAMNUM) == SENTSUCCESSFULLY);
 }
-
-
-
-TEST_CASE("Test for valid Random values")
-{
-  float random_value = 0;
-  float min = 10;
-  float max = 50;
-  random_value = RandomNumGenerator(min,max);
-  REQUIRE(min <= random_value);
-  REQUIRE(max >= random_value);
-}
-
-
-TEST_CASE("Test for invalid Random values")
-{
-  
-  float min = 50;
-  float max = 10;
-  REQUIRE(RandomNumGenerator(min,max) == 0);
-}
-
-
-TEST_CASE("Test for Send BMS data")
-{
-  REQUIRE(BMS_SendData() == SENTSUCCESSFULLY);
-}
-
 
