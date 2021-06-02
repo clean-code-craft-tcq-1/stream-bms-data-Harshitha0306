1f0dff02490063c7ffd72647f806b1e4e033844b
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-06-02 23:41:38 +0530

    Update BMS_DataSender.c

--- BMS_DataSender.c
+++ BMS_DataSender.c
@@ -71,7 +71,7 @@ float RandomNumGenerator(float min, float max)
 
 SendStatus BMS_SendData(struct Battery_Parameter_s Temperature_s,struct Battery_Parameter_s ChargeRate_s,int MAXPARAMNUM)
 {
-   SendStatus Validity_status = SENDING_FAILED,;
+   SendStatus Validity_status = SENDING_FAILED;
    float * Temperature = Get_BMSTemperatue(Temperature_s,MAXPARAMNUM);
    float * ChargeRate = Get_BMSChargeRate(ChargeRate_s,MAXPARAMNUM);
    Validity_status = Output_ToConsole(Temperature,ChargeRate,MAXPARAMNUM);
