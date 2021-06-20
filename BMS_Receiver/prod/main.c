90c19b25f438793911d2bfea6c6b158d2cacb24b
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-06-20 17:56:14 +0530

    Production code with only TESTMODE macro changed

--- BMS_Receiver/prod/main.c
+++ BMS_Receiver/prod/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "bms_Rx.h"
+
+
+void signal_handler(int sig) 
+{ 
+  if (sig == SIGINT)
+  { 
+    sig_caught=1; 
+    printf("got signal");
+  } 
+} 
+int main()
+{
+	
+    if (signal(SIGINT, signal_handler) == SIG_ERR) 
+    { 
+    fprintf(stderr, "signal func registration failed\n"); 
+    return 1; 
+    }
+
+	BMSDataReceiverCalc();
+}
 
