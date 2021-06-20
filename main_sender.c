15cf905d539376b744804a3d0115e9d356860129
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-06-20 21:25:27 +0530

    Update main_sender.c

--- main_sender.c
+++ main_sender.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include "BMS_DataSender.h"
 
-void main()
+int main()
 {
 	  struct Battery_Parameter_s Temperature_s = {20,80};
     struct Battery_Parameter_s ChargeRate_s  = {0,5};
     int MAXPARAMNUM = 30;
     
     BMS_SendData(Temperature_s,ChargeRate_s,MAXPARAMNUM);
+	
+	return 0;
 }
